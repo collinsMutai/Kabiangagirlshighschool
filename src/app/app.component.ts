a57206cb5ca8c4e9440a0d6bff699d9248fc6d54
Collins Mutai <collinsfrontend@gmail.com>
2025-01-20 18:19:36 +0300

    ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ImageSliderComponent } from "./components/image-slider/image-slider.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ImageSliderComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'kabiangagirls';
+  screenWidth: number = window.innerWidth; // Initialize with the current window size
+
+  // Listen for window resize events
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.screenWidth = window.innerWidth; 
+  }
 }
